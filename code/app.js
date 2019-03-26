527cd7207e6b24f787e241c73556e8c65a9a7f37
danilo <ddanilo731@gmail.com>
2019-03-26 09:38:13 +0100

    Aggiunta autenticazione

--- code/app.js
+++ code/app.js
@@ -1,9 +1,29 @@
-var init=function() {
-    console.log("ciao sono Luigi con jquery");
+var entra=function() {
+    console.log("Hai cliccato su accedi");
+    var utente = $("#utente>input").val();
+    console.log("utente: "+ utente);
+    var password = $("#password>input").val();
+    console.log("password: "+ password);
+    var auth = btoa(utente+password);
+    console.log("auth: "+auth,
+    accediRisposta);
+
+    $.get("http://10.3.0.205:3000/login?auth="+auth); //chiamata http
+}    
+ 
+var accediRisposta=function(data){
+    console.log(data);
+    alert(data);
+}
+
+var init=function(){
+    console.log("Ciao");
     $("#utente>input")
-    .attr("placeholder","@gmail.com");
+    .attr("placeholder","e-mail");
     $("#password>input")
-    .attr("placeholder","my password");
+    .attr("placeholder", "password");
+
+    $("#entra>button").on("click",entra);
     
 }
 
